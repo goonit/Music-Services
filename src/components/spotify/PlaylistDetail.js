3e57571817e569bd3f8da11407c0b61dcb70ed39
Herb <cpt.tenneal@gmail.com>
2018-01-07 21:06:40 -0600

    whoops
    forgot some stuff

---
+++ src/components/spotify/PlaylistDetail.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+
+class PlaylistDetail extends Component {
+	state = {};
+	render() {
+		return (
+			<div>
+				<Paper elevation={4} />
+			</div>
+		);
+	}
+}
+
+export default PlaylistDetail;
